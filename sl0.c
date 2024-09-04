0c44171ea684779d2c58d9e912670669a22bc8aa
Ramon Fallon <ramonfallon@gmail.com>
2024-09-04 23:53:07 +0100

    minor change or simple comment

--- sl0.c
+++ sl0.c
@@ -1,7 +1,4 @@
-/* Cairo has a simple bezier (path, I ithink is also has a colour gradient)
- * and it's not called bezier at all, but curve_to() 
- * this zero version of bez, is the eample in
- * https://www.cairographics.org/samples/
+/* Exercises in slope
  *
  * A major mistake you can make is to send a line to the rad*sin(angle) just like that.
  * NO, it must be done relative to the point you moved to.
@@ -41,9 +38,9 @@ int main (int argc, char *argv[])
     printf("cos pi/4=%2.4f\n", cos(M_PI_4));
     double x2=radi*cos(M_PI_4);
     double y2=radi*sin(M_PI_4);
-    cairo_move_to (cr, x, y);
-    cairo_line_to (cr, x+x2, y+y2);
-    cairo_stroke (cr);
+    // cairo_move_to (cr, x, y);
+    // cairo_line_to (cr, x+x2, y+y2);
+    // cairo_stroke (cr);
     
     // try with slope or angle
     cairo_set_source_rgba(cr, 0.35, 0.6, 0.75, 0.6);
@@ -54,6 +51,16 @@ int main (int argc, char *argv[])
     cairo_move_to (cr, x, y);
     cairo_line_to (cr, x+x4, y+y4);
     
+    cairo_move_to (cr, x, y+20);
+    printf("tan pi/6=%2.4f\n", tan(M_PI/6.));
+    float tn=tan(M_PI/6.);
+    cairo_line_to (cr, x+60, y+20+tn*60);
+    cairo_stroke (cr);
+
+    // the perpendicular to the tangent, "take away from x that you previously added to y, what you previously added to x, now add to y"
+    cairo_move_to (cr, x, y+20);
+    cairo_line_to (cr, x-tn*60, y+20+60);
+    cairo_set_source_rgba(cr, 0.85, 0.8, 0.25, 0.6);
     cairo_stroke (cr);
     
     cairo_destroy (cr);
